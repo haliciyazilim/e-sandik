9741247a61420527ef9a8579bcd1c2beab174724
Alperen Kavun <alperen@halici.com.tr>
2012-11-08 11:47:26 +0200

    Initial Commit

---
+++ e-Sandik/SandikSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SandikSecondViewController.h
+//  e-Sandik
+//
+//  Created by Alperen Kavun on 08.11.2012.
+//  Copyright (c) 2012 Halıcı. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SandikSecondViewController : UIViewController
+
+@end
