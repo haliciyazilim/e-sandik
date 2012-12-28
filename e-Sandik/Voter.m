de8f969e544068a927d77f08025d29a91d34e765
Alperen Kavun <alperen@halici.com.tr>
2012-12-28 17:48:02 +0200

    little bug fixed

--- e-Sandik/Voter.m
+++ e-Sandik/Voter.m
@@ -55,8 +55,18 @@
         _school = [kunyeDictionary objectForKey:@"SandikAlani"];
         _chest = [NSString stringWithFormat:@"%@", [kunyeDictionary objectForKey:@"SandikNo"]];
         _chestIndex = [NSString stringWithFormat:@"%@", [kunyeDictionary objectForKey:@"SandikSiraNo"]];
-        _isInformationsOld = [[aDictionary objectForKey:@"EskiListe"] boolValue];
-        _electionYear = [aDictionary objectForKey:@"SecimYili"];
+        if([aDictionary objectForKey:@"EskiListe"]){
+            _isInformationsOld = [[aDictionary objectForKey:@"EskiListe"] boolValue];
+        }
+        else{
+            _isInformationsOld = YES;
+        }
+        if([aDictionary objectForKey:@"SecimYili"]){
+            _electionYear = [aDictionary objectForKey:@"SecimYili"];
+        }
+        else{
+            _electionYear = @"2011";
+        }
         
         
         
